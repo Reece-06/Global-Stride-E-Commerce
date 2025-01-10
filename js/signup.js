a55ae316d4acbfce6d9878cd27bb8fd6dd696e16
Reece De Padua <reecedepadua@gmail.com>
2025-01-10 19:26:20 +0800

    revised signup modal event

--- js/signup.js
+++ js/signup.js
@@ -6,17 +6,28 @@ const signupBtn = document.querySelector(".section-cta__btn");
 
 const profileIcon = document.querySelector(".header__profile");
 const body = document.querySelector("body");
-const signupOverlay = document.querySelector(".signup-overlay");
+
 const signupExit = document.querySelector(".signup__exit");
 
-signupBtn.addEventListener("click", () => {
+const profileBtn = document.querySelector(".header__profile");
+
+function showSignupModal() {
   body.classList.add(removeScroll);
   body.classList.add(openSignupOverlay);
-  signupOverlay.classList.add(openSignupModal);
-});
-
-signupExit.addEventListener("click", () => {
+  body.classList.add(openSignupModal);
+}
+function removeSignupModal() {
   body.classList.remove(removeScroll);
   body.classList.remove(openSignupOverlay);
-  signupOverlay.classList.remove(openSignupModal);
+  body.classList.remove(openSignupModal);
+}
+
+signupBtn.addEventListener("click", () => {
+  showSignupModal();
+});
+profileBtn.addEventListener("click", () => {
+  showSignupModal();
+});
+signupExit.addEventListener("click", () => {
+  removeSignupModal();
 });
