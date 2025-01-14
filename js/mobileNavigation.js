c7103211454c2944b0b455791175725fdb947b53
Reece De Padua <reecedepadua@gmail.com>
2025-01-14 16:42:26 +0800

    fixed subnav for mobile

--- js/mobileNavigation.js
+++ js/mobileNavigation.js
@@ -1,7 +1,17 @@
 const mobileMenu = document.querySelector(".header__menu");
 const closeMobileMenu = document.querySelector(".header__close");
 
+const primarySubNavs = document.querySelectorAll(".header__subnav-primary");
+const secondarySubNavs = document.querySelectorAll(".header__subnav-secondary");
+const tertiarySubNavs = document.querySelectorAll(".header__subnav-tertiary");
+
+const openPrimarySubNav = "header__subnav-primary--open";
+const openSecondarySubNav = "header__subnav-secondary--open";
+const openTertiarySubNav = "header__subnav-tertiary--open";
+
 const openMobileSubNav = "header--subnav-mobile-open";
+const rotateIcon = "header__subnav-icon--rotate";
+const hideArrowBtn = "header__subnav-btn--hide";
 
 mobileMenu.addEventListener("click", () => {
   header.classList.add(openMobileSubNav);
@@ -10,3 +20,19 @@ mobileMenu.addEventListener("click", () => {
 closeMobileMenu.addEventListener("click", () => {
   header.classList.remove(openMobileSubNav);
 });
+
+function addSubNavEventListener(subNavs, openSubNav) {
+  for (const subNav of subNavs) {
+    if (!subNav.nextElementSibling) {
+      subNav.querySelector(".header__subnav-btn").classList.add(hideArrowBtn);
+    }
+    subNav.addEventListener("click", () => {
+      subNav.parentElement.classList.toggle(openSubNav);
+      subNav.querySelector(".icon").classList.toggle(rotateIcon);
+    });
+  }
+}
+
+addSubNavEventListener(primarySubNavs, openPrimarySubNav);
+addSubNavEventListener(secondarySubNavs, openSecondarySubNav);
+addSubNavEventListener(tertiarySubNavs, openTertiarySubNav);
