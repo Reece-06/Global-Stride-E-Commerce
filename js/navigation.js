eead83d8a0aec6c5478e3f3d33e3ac8d441185f0
Reece De Padua <reecedepadua@gmail.com>
2025-01-15 16:39:03 +0800

    added hover effects on links in js

--- js/navigation.js
+++ js/navigation.js
@@ -3,27 +3,31 @@ const openSubNav = "header--subnav-open";
 const header = document.querySelector(".header");
 const navList = document.getElementsByClassName("header__link");
 const subNavDesktops = document.querySelector(".header__subnav");
+const linkBorderBottom = "header__link--border-bot";
+let lastHoveredLi = null;
 
 // Sub Navigation Event Listeners
 
-["click", "mouseover"].forEach((evt) => {
+["click", "mouseover"].forEach((evtType) => {
   for (const nav of navList) {
     nav.addEventListener(
-      evt,
-      () => {
+      evtType,
+      (evt) => {
         header.classList.add(openSubNav);
-        console.log(header.classList);
+        console.log(evt);
+        lastHoveredLi = nav;
       },
       false
     );
   }
 });
+
 subNavDesktops.addEventListener("mouseover", () => {
   header.classList.add(openSubNav);
-  console.log(header.classList);
+  lastHoveredLi.parentElement.classList.add(linkBorderBottom);
 });
 
 subNavDesktops.addEventListener("mouseout", () => {
   header.classList.remove(openSubNav);
-  console.log(header.classList);
+  lastHoveredLi.parentElement.classList.remove(linkBorderBottom);
 });
