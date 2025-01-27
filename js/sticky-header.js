8e53767e4a18d47adc6b72d2b454934f08f73283
Reece De Padua <reecedepadua@gmail.com>
2025-01-27 21:06:29 +0800

    fixed more details and sticky header

---
+++ js/sticky-header.js
@@ -0,0 +1,19 @@
+const hero = document.querySelector(".section-hero");
+const bodyEl = document.querySelector("body");
+const stickyHeader = "sticky";
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+    if (!ent.isIntersecting) {
+      bodyEl.classList.add(stickyHeader);
+    } else {
+      bodyEl.classList.remove(stickyHeader);
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+  }
+);
+obs.observe(hero);
